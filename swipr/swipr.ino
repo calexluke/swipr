28b5e34ceeacabbde4e70a32ed293dc5e9d1fce6
calexluke <c.alexluke@gmail.com>
2021-03-22 15:35:25 -0500

    Change pin numbers to run on NANO instead of UNO. Add comments to arduino code.

--- swipr/swipr.ino
+++ swipr/swipr.ino
@@ -1,19 +1,25 @@
 #include <SoftwareSerial.h>
 
-const int LED_0 = 3;
-const int LED_1 = 4;
+// assign output pins for relays
+const int RELAY_0 = 8;
+const int RELAY_1 = 7;
+
+// message to be recieved over BlueTooth
 String message;
 
-// Don't use the rx, tx pins on Arduino (0 and 1) - those are for hardware serial.
-SoftwareSerial HM10(11, 12); // RX, TX
-
+// Pass in pins to use as RX and TX on arduino.
+// Don't use the labeled RX, TX pins on Arduino (0 and 1) - those are for hardware serial.
+// Arduino RX connects to HM10 TX, Arduino TX connects to HM10 RX.
+// Need Voltage divider for HM10 RX, which runs on 3.4V - arduino sends out 5V signal.
+// Use 1kOhm and 2kOhm resistors for the voltage divider.
+SoftwareSerial HM10(3, 4);
 
 void setup() {
   
   Serial.begin(9600);
   HM10.begin(9600);
-  pinMode(LED_0, OUTPUT);
-  pinMode(LED_1, OUTPUT);
+  pinMode(RELAY_0, OUTPUT);
+  pinMode(RELAY_1, OUTPUT);
   
   Serial.println("Enter AT command here, or control device from phone.");
   Serial.println("Phone commands: 00, 01, 10, or 11.");
@@ -22,13 +28,14 @@ void setup() {
 
 void loop() {
 
-  // Read from HM-10 and send to Serial Monitor
+  // Read from HM-10 and react to the incoming message
   if (HM10.available()) {
     message = HM10.readStringUntil('\n');
     respondToMessage(message);
   }
 
   // Read from Serial Monitor and send to HM-10
+  // Use for AT commands to configure HM-10 (see HM-10 datasheet)
   if (Serial.available()) {
     HM10.write(Serial.read());
   }
@@ -39,28 +46,32 @@ void loop() {
 void respondToMessage(String message) {
   
   if (message.startsWith("OK")) {
-    // status update from HM10
+    // status message from HM10
     Serial.println(message);
     
   } else if (message == "00") {
+    // turn off both relays
     Serial.println("Changing to state " + message);
-    digitalWrite(LED_0, LOW);
-    digitalWrite(LED_1, LOW);
+    digitalWrite(RELAY_0, LOW);
+    digitalWrite(RELAY_1, LOW);
     
   } else if (message == "01") {
+    // turn on relay 1, turn off relay 0
     Serial.println("Changing to state " + message);
-    digitalWrite(LED_0, LOW);
-    digitalWrite(LED_1, HIGH);
+    digitalWrite(RELAY_0, LOW);
+    digitalWrite(RELAY_1, HIGH);
     
   } else if (message == "10") {
+    // turn on relay 0, turn off relay 1
     Serial.println("Changing to state " + message);
-    digitalWrite(LED_0, HIGH);
-    digitalWrite(LED_1, LOW);
+    digitalWrite(RELAY_0, HIGH);
+    digitalWrite(RELAY_1, LOW);
     
   } else if (message == "11") {
+    // turn on both relays
     Serial.println("Changing to state " + message);
-    digitalWrite(LED_0, HIGH);
-    digitalWrite(LED_1, HIGH);
+    digitalWrite(RELAY_0, HIGH);
+    digitalWrite(RELAY_1, HIGH);
     
   } else {
     Serial.println(message + " is not a valid command.");
